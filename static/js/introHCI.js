b41db3f4d7c3595918f6a857932c51371bc28515
Michelle Nguyen <mnn027@ucsd.edu>
2018-01-24 22:53:45 -0800

    Controlled content visibility

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,4 +15,28 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Michelle is Really Cool");
+		$("#testjs").addClass("active");
+		$("#testjs").text("Read the above...")
+	});
+}
+
+function projectClick(e) {
+	//prevent page from reloading
+	e.preventDefault();
+
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeOut();
+    }
+
 }
